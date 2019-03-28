2107e4efdb2915a5f6b06408b2130c1a0c7b18ae
rodrigo <rodrigo1987macedo@gmail.com>
2019-03-28 09:03:43 -0300

    index page and linked test page

---
+++ pages/test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+function test() {
+	return (
+		<Layout title='Test title' description='A description about the test page'>
+			<p>jojo</p>
+		</Layout>
+	)
+}
+
+export default test
